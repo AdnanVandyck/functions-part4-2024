917afdabdde591512763dec0b0e69ac80d9a5b3e
Adnan <adnanvandyck@gmail.com>
2024-04-15 02:29:45 -0400

    added mult list

--- functions_part4.py
+++ functions_part4.py
@@ -4,3 +4,20 @@ def max_num(a,b,c):
 print(max_num(1,2,3))
 print(max_num(100,50,1))
 print(max_num(15,30,2))
+
+def mult_list(lst): 
+    if len(lst) == 0:
+        return 0
+    
+    prod = lst[0]
+
+    if len(lst) > 1:
+        for i in lst[1:]:
+            prod = prod * i
+
+            return prod
+        
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list[15])
+
